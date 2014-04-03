7780eb8521b8db7547b5d69c07a093158b8ad3a7
Eric Stensland <estensland88@gmail.com>
2014-04-03 15:57:17 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ericrstensland::Application.config.secret_key_base = '4670d77800e81bd64687ce5a1a5fe901ce47c9eda8630f719c2f5e3d95a80fcfb3f79fca5c3a6f33ff59bd5ae4878f3ea6007fa466e8b80a97a2ee90f6dd6ed2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ericrstensland::Application.config.session_store :cookie_store, key: '_ericrstensland_session'

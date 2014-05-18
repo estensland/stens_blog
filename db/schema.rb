3b49b5c19c7df38ccbb05b1a636d58b0248f3486
Eric Stensland <estensland88@gmail.com>
2014-05-18 11:35:30 -0500

    random updated files, schema, seeds, etc

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516130044) do
+ActiveRecord::Schema.define(version: 20140516130046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: true do |t|
-    t.string "title"
-    t.text   "body"
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts_tags", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "tag_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

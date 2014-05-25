4426de7ab443d18daa294f512233b0b5412dcc38
Eric Stensland <estensland88@gmail.com>
2014-05-25 14:20:21 -0500

    re working css

--- app/controllers/concerns/tags_controller.rb
+++ app/controllers/concerns/tags_controller.rb
@@ -1,12 +1,12 @@
 class TagsController < ApplicationController
   def index
     @blog_view = true
-    @tags = Tag.all.sort_by(&:id).reverse
+    @tags = Tag.all.sort_by(&:id)
   end
 
   def show
     @blog_view = true
     @tag = Tag.find(params[:id])
-    @posts = @tag.posts
+    @posts = @tag.posts.sort_by(&:id).reverse
   end
 end

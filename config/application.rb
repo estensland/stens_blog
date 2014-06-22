1122b4cb341994295cbc7b568eb4226471c87e3a
Eric Stensland <estensland88@gmail.com>
2014-06-21 19:34:12 -0500

    moved things

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 module Ericrstensland
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -15,7 +14,8 @@ module Ericrstensland
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-
+    config.assets.initialize_on_precompile = false
+    
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
